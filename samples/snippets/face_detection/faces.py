f54c7bfa32dce820951dc4feee2120945c69256a
Gus Class <gguuss@gmail.com>
2017-03-28 15:04:03 -0700

    Vision face tutorial [(#880)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/880)
    * Updates sample to use the Cloud client library
    
    * Nits found after commit
    
    * Nudge for travis
    
    * flake8 hates my face

---
+++ samples/snippets/face_detection/faces.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Draws squares around detected faces in the given image."""
+
+import argparse
+
+from google.cloud import vision
+from PIL import Image, ImageDraw
+
+
+def detect_face(face_file, max_results=4):
+    """Uses the Vision API to detect faces in the given file.
+
+    Args:
+        face_file: A file-like object containing an image with faces.
+
+    Returns:
+        An array of Face objects with information about the picture.
+    """
+    content = face_file.read()
+    # [START get_vision_service]
+    image = vision.Client().image(content=content)
+    # [END get_vision_service]
+
+    return image.detect_faces()
+
+
+def highlight_faces(image, faces, output_filename):
+    """Draws a polygon around the faces, then saves to output_filename.
+
+    Args:
+      image: a file containing the image with the faces.
+      faces: a list of faces found in the file. This should be in the format
+          returned by the Vision API.
+      output_filename: the name of the image file to be created, where the
+          faces have polygons drawn around them.
+    """
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(bound.x_coordinate, bound.y_coordinate)
+               for bound in face.bounds.vertices]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    im.save(output_filename)
+
+
+def main(input_filename, output_filename, max_results):
+    with open(input_filename, 'rb') as image:
+        faces = detect_face(image, max_results)
+        print('Found {} face{}'.format(
+            len(faces), '' if len(faces) == 1 else 's'))
+
+        print('Writing to file {}'.format(output_filename))
+        # Reset the file pointer, so we can read the file again
+        image.seek(0)
+        highlight_faces(image, faces, output_filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Detects faces in the given image.')
+    parser.add_argument(
+        'input_image', help='the image you\'d like to detect faces in.')
+    parser.add_argument(
+        '--out', dest='output', default='out.jpg',
+        help='the name of the output file.')
+    parser.add_argument(
+        '--max-results', dest='max_results', default=4,
+        help='the max results of face detection.')
+    args = parser.parse_args()
+
+    main(args.input_image, args.output, args.max_results)
