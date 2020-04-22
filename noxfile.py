e067d8a283aaba44d9da7147c9ded5c8ab0e536c
yoshi-automation <yoshi-automation@google.com>
2020-04-22 05:27:34 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- noxfile.py
+++ noxfile.py
@@ -72,6 +72,7 @@ def default(session):
     session.run(
         "py.test",
         "--quiet",
+        "--cov=google.cloud.vision",
         "--cov=google.cloud",
         "--cov=tests.unit",
         "--cov-append",
@@ -110,9 +111,8 @@ def system(session):
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
     session.install("mock", "pytest")
-
-    session.install("google-cloud-storage")
-    session.install("-e", "test_utils")
+    session.install("git+https://github.com/googleapis/python-test-utils")
+    session.install("-e", "../storage")
     session.install("-e", ".")
 
     # Run py.test against the system tests.
@@ -130,7 +130,7 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
-    session.run("coverage", "report", "--show-missing", "--fail-under=99")
+    session.run("coverage", "report", "--show-missing", "--fail-under=100")
 
     session.run("coverage", "erase")
 
@@ -140,7 +140,7 @@ def docs(session):
     """Build the docs for this library."""
 
     session.install("-e", ".")
-    session.install("sphinx", "alabaster", "recommonmark")
+    session.install("sphinx<3.0.0", "alabaster", "recommonmark")
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
