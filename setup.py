fd5c7bc14d1a959ba8291043cf29de8b3bd519ea
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-07-19 16:03:52 -0600

    fix(deps): pin 'google-{api,cloud}-core', 'google-auth' to allow 2.x versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ description = "Cloud Vision API API client library"
 version = "2.4.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.26.0, <2.0.0dev",
+    "google-api-core[grpc] >= 1.26.0,  <3.0.0dev",
     "proto-plus >= 1.15.0",
     "packaging >= 14.3",
 ]
