944b7de5c484b6aea7844649d15c6576a8cb1620
Anthonios Partheniou <partheniou@google.com>
2022-06-08 22:59:53 +0000

    fix(deps): require protobuf<4.0.0 on v1 branch

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
     "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     'enum34; python_version < "3.4"',
+    "protobuf<4.0.0dev",
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
